19fe0ee5415e53edf64bcea645ade465a24b74fc
Erkin Kubatov <qoobatov@gmail.com>
2023-02-26 20:16:43 +0600

    create dinamyc head and add single styles for allfiles

--- app.js
+++ app.js
@@ -1,40 +1,63 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const morgan = require("morgan");
+
 
 const PORT = 3000;
 
 app.set("view engine", "ejs");
 
+const creatrePath = (page) =>
+  path.resolve(__dirname, "ejs-views", `${page}.ejs`); // функция пути
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`Listening port ${PORT}`);
 });
 
-const creatrePath = (page) => path.resolve(__dirname, "ejs-views", `${page}.ejs`); // функция пути
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
-// не понял про middleware, есть еще шаблонизатор ejs
+app.use(express.static('styles'));
+
+
+
+
+
 
 // ниже роутинги с использованием express
 app.get("/", (req, res) => {
-  res.render(creatrePath("index"));
+  const title = "Home";
+  res.render(creatrePath("index"), { title });
 });
 app.get("/contacts", (req, res) => {
-  res.render(creatrePath("contacts"));
+  const title = "Contacts";
+  const contacts = [
+    { name: "Github", link: "http://github.com/qoobatov" },
+    { name: "Twitter", link: "http://twitter.com/home" },
+  ];
+  res.render(creatrePath("contacts"), { contacts, title });
 });
+
 app.get("/posts/:id", (req, res) => {
-  res.render(creatrePath("post"));
+  const title = "Posts";
+  res.render(creatrePath("post"), { title });
 });
+
 app.get("/posts", (req, res) => {
-  res.render(creatrePath("posts"));
+  const title = "Posts";
+  res.render(creatrePath("posts"), { title });
 });
+
 app.get("/add-post", (req, res) => {
-  res.render(creatrePath("add-post"));
+  const title = "add-post";
+  res.render(creatrePath("add-post"), { title });
 });
 app.get("/about-us", (req, res) => {
+  const title = "contacts";
   res.redirect("/contacts");
 });
 app.use((req, res) => {
+  const title = "error";
   // именно в эксперсс вот этот перехватчик ошибок use должен быть в самом конце, потому что, после него никакие другие роутинги или редиректы не будут работать(это особенность экспресс)
-  res.status(404).render(creatrePath("error"));
+  res.status(404).render(creatrePath("error"), { title });
 });
